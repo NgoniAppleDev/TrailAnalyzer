208311ef48f118f4218485bfdf318241c6be6332
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-07-15 14:23:41 +0200

    Initial Commit

---
+++ TrailAnalyzer/TrailAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailAnalyzerApp.swift
+//  TrailAnalyzer
+//
+//  Created by Ngoni Katsidzira  on 15/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct TrailAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
